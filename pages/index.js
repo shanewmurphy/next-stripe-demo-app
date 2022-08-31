21d033b817f682d46395b5f536cf14f35e3b34a1
shanewmurphy <shn.murphy@gmail.com>
2022-08-31 14:07:13 +0100

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -12,43 +12,32 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <h1 className={styles.title}>Choose your lessons</h1>
+        <div className={styles.price_container}>
+          <div className={styles.price_card_item}>
+            <h1>10</h1>
+            <h3>Weeks</h3>
+            <div className={styles.img_svg_container}></div>
+            <div className={styles.btn_container}>
+              <button>Buy Now</button>
+            </div>
+          </div>
+          <div className={styles.price_card_item}>
+            <h1>20</h1>
+            <h3>Weeks</h3>
+            <div className={styles.img_svg_container}></div>
+            <div className={styles.btn_container}>
+              <button>Buy Now</button>
+            </div>
+          </div>
+          <div className={styles.price_card_item}>
+            <h1>30</h1>
+            <h3>Weeks</h3>
+            <div className={styles.img_svg_container}></div>
+            <div className={styles.btn_container}>
+              <button>Buy Now</button>
+            </div>
+          </div>
         </div>
       </main>
 
@@ -58,12 +47,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
